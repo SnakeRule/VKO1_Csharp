647c9e28adc4bba3a133ae454fbda88fa830a9d4
Jere Valtanen <jerevaltanen@msn.com>
2016-01-13 14:14:09 +0200

    Tehtävä 16 lisätty

---
+++ Harjoitus 15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            int input_half;
+
+            Console.Write("Enter a number");
+            input = int.Parse(Console.ReadLine());
+            input_half = input / 2;
+
+            for(int i = 0; i != input; i++)
+            {
+                for (int o = input_half; o != 0; o--)
+                {
+                    Console.Write(" ");
+   
+                }
+
+                for(int u = 0; u != i; u++)
+                {
+                    Console.Write("*");
+                }
+                input_half--;
+                Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Harjoitus 16/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int rndvalue = random.Next(0, 100);
+            int input;
+            bool loop = true;
+
+            do
+            {
+                Console.Write("Guess the number 0-100 : ");
+                input = int.Parse(Console.ReadLine());
+
+                if (input == rndvalue)
+                {
+                    Console.WriteLine("Correct!");
+                        loop = false;
+                }
+                else if (input < rndvalue)
+                {
+                    Console.WriteLine("The number is bigger!");
+                }
+                else if (input > rndvalue)
+                {
+                    Console.WriteLine("The number is smaller!");
+                }
+
+            } while (loop == true);
+
+            Console.Read();
+        }
+    }
+}
